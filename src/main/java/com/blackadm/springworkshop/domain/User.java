e13724e655456b074f773557cd89a53ab8039ca1
black-adm <mattheuscontato17@gmail.com>
2023-10-12 12:04:17 -0300

    Resource method and update user entity

--- src/main/java/com/blackadm/springworkshop/domain/User.java
+++ src/main/java/com/blackadm/springworkshop/domain/User.java
@@ -1,8 +1,58 @@
 package com.blackadm.springworkshop.domain;
 
-public class User {
+import java.io.Serializable;
+import java.util.Objects;
 
-    private String id;
+public class User implements Serializable {
+
+    private int id;
     private String name;
     private String email;
+
+    public User() {
+    }
+
+    public User(int id, String name, String email) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
