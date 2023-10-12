0141a51102a1a0c214d04dbd41083b2d1f49ed7d
black-adm <mattheuscontato17@gmail.com>
2023-10-12 13:26:51 -0300

    Add Dto

--- src/main/java/com/blackadm/springworkshop/resources/UserResource.java
+++ src/main/java/com/blackadm/springworkshop/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.blackadm.springworkshop.resources;
 
 import com.blackadm.springworkshop.domain.User;
+import com.blackadm.springworkshop.dtos.UserDto;
 import com.blackadm.springworkshop.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/users")
@@ -18,8 +20,9 @@ public class UserResource {
     private UserService userService;
 
     @GetMapping
-    public ResponseEntity<List <User>> findAll() {
+    public ResponseEntity<List <UserDto>> findAll() {
         List<User> list = userService.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDto> listDto = list.stream().map(x -> new UserDto(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
